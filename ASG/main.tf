3b7be53238a7fae3ca59acbbabc2728999eca7a0
cuneytoghan <coghann@gmail.com>
2023-07-19 03:06:14 +0000

    eadded target

--- ASG/main.tf
+++ ASG/main.tf
@@ -34,7 +34,7 @@ resource "aws_elb" "bar" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80/"
+    target              = "TCP:80"
     interval            = 30
   }
 
