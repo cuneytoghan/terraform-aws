f583312954c8d200c8ae0f2d1ff3ed72fffb25e6
cuneytoghan <coghann@gmail.com>
2023-07-19 02:56:39 +0000

    added elb

--- ASG/main.tf
+++ ASG/main.tf
@@ -16,3 +16,36 @@ resource "aws_autoscaling_group" "bar" {
     version = "$Latest"
   }
 }
+# Create a new load balancer
+resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elb"
+  availability_zones = ["us-east-1a"]
+
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+}
+# Create a new load balancer attachment
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = aws_autoscaling_group.bar.id
+  elb                    = aws_elb.bar.id
+}
